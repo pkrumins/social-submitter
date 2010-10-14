d3a8d87a15cc4336ed7259cf1cbf9d1e9da8f297
Peteris Krumins <peteris.krumins@gmail.com>
2010-10-14 19:09:24 +0300

    added fileRouter and username, password error checking

--- submitter.js
+++ submitter.js
@@ -2,20 +2,35 @@ var DNode = require('dnode');
 var express = require('express');
 var fs = require('fs');
 
+function fileRouter(filename, mime) {
+    var mime = mime || 'text/html';
+    return function (req, res) {
+        fs.readFile(filename, function (err, data) {
+            if (!err) {
+                res.send(data, { 'Content-Type' : mime });
+            }
+        });
+    }
+}
+
 var app = express.createServer();
-app.get('/', function (req, res) {
-    var html = fs.readFileSync(__dirname + '/html/submitter.html');
-    res.send(html, { 'Content-Type': 'text/html' });
-});
+app.get('/', fileRouter(__dirname + '/html/submitter.html'));
 app.get('/dnode.js', require('dnode/web').route());
 app.get('/form_:name', function (req, res) {
-    var html = fs.readFileSync(__dirname + '/html/form_' + req.params.name + '.html');
-    res.send(html, { 'Content-Type': 'text/html' });
+    var filename = __dirname + '/html/form_' + req.params.name + '.html';
+    (fileRouter(filename))(req, res);
 });
 
 DNode(function (client) {
     this.submit = function (data) {
-        console.dir(data);
+        if (!data.username.length) {
+            client.error('Username is empty.');
+            return;
+        }
+        if (!data.password.length) {
+            client.error('Password is empty.');
+            return;
+        }
     }
 }).listen(app, {
     transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/),
